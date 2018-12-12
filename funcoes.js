86bdf1cb60932f02264e8635eed7044bf3fd0980
Paulo Henrique Mello dos Santos <PHMSantos@agf.com.br>
2018-12-12 11:13:27 -0200

    Novo Botao

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,11 @@
 function mostrar(){
 	alert('Mensagem Github');
 }
+function mostrar2(){
+	alert('Allianz - Nova Mensagem');
+}
 var btn = document.getElementById("botao");
 btn.addEventListener('click', mostrar);
+
+var bt2 = document.getElementById("botao2");
+bt2.addEventListener('click', mostrar2);
